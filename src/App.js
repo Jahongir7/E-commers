8c9f750aee9debe1f25547b1072e641dec5d09c5
Jahongir7 <Jahongir95119@gmail.com>
2021-07-25 17:09:00 +0500

    new project added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import React from 'react';
-import Header from './components/Header'
-import Products from './components/Products'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import {DataProvider} from './components/DataProvider'
-import Details from './components/Details'
-import Cart from './components/Cart'
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <DataProvider>
-      <div className="App">
-        <Router>
-          <Header />
-
-          <section>
-            <Routes>
-              <Route path="products" element={ <Products /> } />
-              <Route path="products/:id" element={ <Details /> } />
-              <Route path="cart" element={ <Cart /> } />
-            </Routes>
-          </section>
-        </Router>
-      </div>
-    </DataProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
