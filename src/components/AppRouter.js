3768f901d593f94b2763bb70b10366b857298ff3
Jahongir7 <Jahongir95119@gmail.com>
2021-08-06 19:42:14 +0500

    react-firebase-hooks added

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,9 +1,13 @@
 import { Route, Switch, Redirect } from "react-router-dom";
 import { privateRoutes, publicRoutes } from "../routes";
 import { CHAT_ROUTE, LOGIN_ROUTE } from "../utils/const";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useContext } from "react";
+import { Context } from "..";
 
 function AppRouter() {
-  const user = false;
+  const { auth } = useContext(Context);
+  const [user] = useAuthState(auth);
 
   return user ? (
     <Switch>
