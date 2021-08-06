3abe5a44140bba05fb03fa6ec37be98647459c3f
Jahongir7 <Jahongir95119@gmail.com>
2021-08-06 18:19:18 +0500

    key={path} this is single key

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -8,14 +8,14 @@ function AppRouter() {
   return user ? (
     <Switch>
       {privateRoutes.map(({ path, Component }) => (
-        <Route path={path} component={Component} exact={true} />
+        <Route key={path} path={path} component={Component} exact={true} />
       ))}
       <Redirect to={CHAT_ROUTE} />
     </Switch>
   ) : (
     <Switch>
       {publicRoutes.map(({ path, Component }) => (
-        <Route path={path} component={Component} exact={true} />
+        <Route key={path} path={path} component={Component} exact={true} />
       ))}
       <Redirect to={LOGIN_ROUTE} />
     </Switch>
