dab57f3f900affcce335fb1af37e9b71583e3ec8
Jahongir7 <Jahongir95119@gmail.com>
2021-08-06 18:17:58 +0500

    Firebase added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { createContext } from "react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+firebase.initializeApp({
+  apiKey: "AIzaSyCxLuesHslTYEADlr1TK0IjK2zl695xKb0",
+  authDomain: "jahongir-35a83.firebaseapp.com",
+  projectId: "jahongir-35a83",
+  storageBucket: "jahongir-35a83.appspot.com",
+  messagingSenderId: "825933590700",
+  appId: "1:825933590700:web:34ae20411d4b2b22dd6f09",
+  measurementId: "G-PN5PMBYP96",
+});
+export const Context = createContext(null);
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+ReactDOM.render(
+  <Context.Provider value={{ firestore, auth, firebase }}>
+    <App />
+  </Context.Provider>,
+  document.getElementById("root")
+);
