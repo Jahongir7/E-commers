091a403c897f8cff1ea1183caa4282bca651141c
Jahongir7 <Jahongir95119@gmail.com>
2021-08-08 21:00:12 +0500

    new project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import AppRouter from "./components/AppRouter";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useContext } from "react";
-import { Context } from ".";
-import Loader from "./components/Loader";
 
 function App() {
-  const { auth } = useContext(Context);
-  // eslint-disable-next-line no-unused-vars
-  const [user, loading, error] = useAuthState(auth);
-  if (loading) {
-    return <Loader />;
-  }
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <AppRouter />
-    </BrowserRouter>
-  );
+  return <div>Hello world</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-import { createContext } from "react";
+import ReactDOM from "react-dom";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCxLuesHslTYEADlr1TK0IjK2zl695xKb0",
-  authDomain: "jahongir-35a83.firebaseapp.com",
-  projectId: "jahongir-35a83",
-  storageBucket: "jahongir-35a83.appspot.com",
-  messagingSenderId: "825933590700",
-  appId: "1:825933590700:web:34ae20411d4b2b22dd6f09",
-  measurementId: "G-PN5PMBYP96",
-});
-export const Context = createContext(null);
-const auth = firebase.auth();
-const firestore = firebase.firestore();
 ReactDOM.render(
-  <Context.Provider value={{ firestore, auth, firebase }}>
+  <React.StrictMode>
     <App />
-  </Context.Provider>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
